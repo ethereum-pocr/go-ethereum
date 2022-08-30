c32f33b1044cd8936fe8468d78f68c650d3a4b0c
IXXOROOT <sebastien@ixxo.io>
2022-08-30 11:20:17 +0200

    Temp dev on the latest branch

---
+++ core/systemcontracts/const.go
@@ -0,0 +1,7 @@
+package systemcontracts
+
+const (
+	// genesis contracts
+	proofOfCarbonReductionContractAddress = "0x0000000000000000000000000000000000000100"
+	sessionVariablesContractAddress       = "0x0000000000000000000000000000000000000101"
+)
