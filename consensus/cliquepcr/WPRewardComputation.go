716c84c1587f651badc6c7e7a7dca1d8ab70d357
sjehan <sjehan@finaxys.com>
2022-10-10 18:03:06 +0200

    Creating modular dependency for reward computation

---
+++ consensus/cliquepcr/WPRewardComputation.go
@@ -0,0 +1,174 @@
+package cliquepcr
+import (
+	"errors"
+	"math/big"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+)
+type WPRewardComputation struct {
+}
+
+func (wp *WPRewardComputation) CalculateAcceptNewSealersReward(nbNodes *big.Int) (*big.Int, error) {
+	// no additional reward when there is one node or less
+	one := big.NewInt(1)
+	if nbNodes.Cmp(one) <= 0 {
+		return zero, nil
+	}
+	// N = nbNodes - 1
+	N := new(big.Rat).SetInt(nbNodes)
+	N = N.Sub(N, big.NewRat(1, 1))
+	// reward = (N-1)/3
+	rew := big.NewRat(1, 3)
+	rew = rew.Mul(N, rew)
+	// reward = (N-1)/3 * CTC Unit
+	rew = rew.Mul(rew, new(big.Rat).SetInt(CTCUnit))
+	// calculate the result rounding to the unit
+	rewI := new(big.Int).Quo(rew.Num(), rew.Denom())
+	return rewI, nil
+}
+
+func (wp *WPRewardComputation) CalculateGlobalInflationControlFactor(M *big.Int) (*big.Rat, error){
+	// L = M / (8 000 000 * 30 / 3) // as integer value
+	// D = 2^L // The divisor : 2 at the power of L
+	// GlobalInflationControl = 1/D // 1; 1/2; 1/4; 1/8 ....
+
+	// If there is no crpto created, return 1
+	if M.Cmp(zero) == 0 {
+		return big.NewRat(1, 1), nil
+	}
+	C := big.NewInt(8_000_000 * 30 / 3)
+	C = C.Mul(C, CTCUnit)
+	L := new(big.Rat).SetFrac(M, C)
+	L2 := new(big.Int).Quo(L.Num(), L.Denom()).Uint64()
+	// D = 2^L
+	D := int64(1) << L2
+	// log.Info("Trace CalculateGlobalInflationControlFactor", "M", M, "L2", L2, "D", D)
+	if D == 0 { // The divisor has reached such a large amount (2^63) than the shift gave 0, So Dividing by a very large number is equivalent to 0
+		return big.NewRat(0, 1), nil
+	}
+	return big.NewRat(1, D), nil
+}
+
+func (wp *WPRewardComputation) CalculatePoCRReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, totalCryptoAmount *big.Int) (*big.Int, error){
+
+	cf, err := wp.CalculateCarbonFootprintReward(nbNodes, totalFootprint, footprint)
+	if err != nil {
+		return nil, err
+	}
+
+	// ns, err := CalculateAcceptNewSealersReward(nbNodes)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	infl, err := wp.CalculateGlobalInflationControlFactor(totalCryptoAmount)
+	if err != nil {
+		return nil, err
+	}
+	// Reward(n, b) = CarbonReduction(n) * N * GlobalInflationControl(b)
+	rew := new(big.Rat).SetInt(cf)
+	// rew = rew.Add(rew, new(big.Rat).SetInt(ns))
+	rew = rew.Mul(rew, new(big.Rat).SetInt(nbNodes))
+	rew = rew.Mul(rew, infl)
+
+	rewI := new(big.Int).Quo(rew.Num(), rew.Denom())
+	return rewI, nil
+}
+func (wp *WPRewardComputation) CalcCarbonFootprintReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, address common.Address, config *params.ChainConfig, state *state.StateDB, header *types.Header) (*big.Int, error){
+	if nbNodes.Cmp(zero) == 0 {
+		return nil, errors.New("cannot average with zero node")
+	}
+	if totalFootprint.Cmp(zero) <= 0 {
+		return nil, errors.New("cannot proceed with zero or negative total footprint")
+	}
+	if footprint.Cmp(zero) <= 0 {
+		return nil, errors.New("cannot proceed with zero or negative footprint")
+	}
+	// average = totalFootprint / nbNodes
+	average := new(big.Rat).SetFrac(totalFootprint, nbNodes)
+	// ratio = nbNodes / totalFootprint
+	ratio := new(big.Rat).Inv(average)
+	// ratio = footprint * (nbNodes / totalFootprint) = X
+	ratio = ratio.Mul(ratio, new(big.Rat).SetInt(footprint))
+	// ratio = X + 0,2
+	ratio = ratio.Add(ratio, big.NewRat(2, 10))
+	// ratio = 1 / (X + 0,2)
+	ratio = ratio.Inv(ratio)
+	// ratio = 1 / (X + 0,2) - 0,5
+	ratio = ratio.Sub(ratio, big.NewRat(5, 10))
+	if ratio.Sign() <= 0 {
+		return big.NewInt(0), nil
+	}
+	// reward = 1 CTC (10^18 Wei)
+	reward := new(big.Rat).SetInt(CTCUnit)
+	// reward = ratio * CTC unit
+	reward = reward.Mul(reward, ratio)
+	// convert to big.Int
+	rewardI := new(big.Int).Quo(reward.Num(), reward.Denom())
+	// cap to 2 CTC units
+	cap := big.NewInt(2)
+	cap = cap.Mul(cap, CTCUnit)
+	if rewardI.Cmp(cap) > 0 {
+		rewardI = cap
+	}
+
+	return rewardI, nil
+}
+
+func (wp *WPRewardComputation) CalculateCarbonFootprintReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int) (*big.Int, error) {
+	if nbNodes.Cmp(zero) == 0 {
+		return nil, errors.New("cannot average with zero node")
+	}
+	if totalFootprint.Cmp(zero) <= 0 {
+		return nil, errors.New("cannot proceed with zero or negative total footprint")
+	}
+	if footprint.Cmp(zero) <= 0 {
+		return nil, errors.New("cannot proceed with zero or negative footprint")
+	}
+	// average = totalFootprint / nbNodes
+	average := new(big.Rat).SetFrac(totalFootprint, nbNodes)
+	// ratio = nbNodes / totalFootprint
+	ratio := new(big.Rat).Inv(average)
+	// ratio = footprint * (nbNodes / totalFootprint) = X
+	ratio = ratio.Mul(ratio, new(big.Rat).SetInt(footprint))
+	// ratio = X + 0,2
+	ratio = ratio.Add(ratio, big.NewRat(2, 10))
+	// ratio = 1 / (X + 0,2)
+	ratio = ratio.Inv(ratio)
+	// ratio = 1 / (X + 0,2) - 0,5
+	ratio = ratio.Sub(ratio, big.NewRat(5, 10))
+	if ratio.Sign() <= 0 {
+		return big.NewInt(0), nil
+	}
+	// reward = 1 CTC (10^18 Wei)
+	reward := new(big.Rat).SetInt(CTCUnit)
+	// reward = ratio * CTC unit
+	reward = reward.Mul(reward, ratio)
+	// convert to big.Int
+	rewardI := new(big.Int).Quo(reward.Num(), reward.Denom())
+	// cap to 2 CTC units
+	cap := big.NewInt(2)
+	cap = cap.Mul(cap, CTCUnit)
+	if rewardI.Cmp(cap) > 0 {
+		rewardI = cap
+	}
+	// no additional reward when there is one node or less
+	one := big.NewInt(1)
+	if nbNodes.Cmp(one) <= 0 {
+		return zero, nil
+	}
+	// N = nbNodes - 1
+	N := new(big.Rat).SetInt(nbNodes)
+	N = N.Sub(N, big.NewRat(1, 1))
+	// reward = (N-1)/3
+	rew := big.NewRat(1, 3)
+	rew = rew.Mul(N, rew)
+	// reward = (N-1)/3 * CTC Unit
+	rew = rew.Mul(rew, new(big.Rat).SetInt(CTCUnit))
+	// calculate the result rounding to the unit
+	rewI := new(big.Int).Quo(rew.Num(), rew.Denom())
+	return rewI, nil
+
+}
--- consensus/cliquepcr/cliquepocr.go
+++ consensus/cliquepcr/cliquepocr.go
@@ -22,7 +22,6 @@ import (
 	"math/big"
 	"sync"
 	"time"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/clique"
@@ -35,6 +34,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rpc"
 	lru "github.com/hashicorp/golang-lru"
+	// "github.com/ethereum/go-ethereum/cliquepcr/rewardcomputation"
 )
 
 // We put new constants to be able to override default clique values
@@ -54,14 +54,12 @@ var (
 
 // address of the PoCR smart contract, with the governance, the footprint, the auditors and the auditor's pledged amount
 var proofOfCarbonReductionContractAddress = "0x0000000000000000000000000000000000000100"
-
 // Use a separate address for collecting the total crypto generated because the smart contract also needs to hold auditor pledge
 var sessionVariablesContractAddress = "0x0000000000000000000000000000000000000101"
-
 var sessionVariableTotalPocRCoins = "GeneratedPocRTotal"
 var zero = big.NewInt(0)
 var CTCUnit = big.NewInt(1e+18)
-
+var rewardComputation WPRewardComputation
 type CliquePoCR struct {
 	config *params.CliqueConfig // Consensus engine configuration parameters
 	db     ethdb.Database       // Database to store and retrieve snapshot checkpoints
@@ -281,119 +279,6 @@ func calcCarbonFootprintReward(address common.Address, config *params.ChainConfi
 	return reward, nil
 }
 
-func CalculatePoCRReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, totalCryptoAmount *big.Int) (*big.Int, error) {
-
-	cf, err := CalculateCarbonFootprintReward(nbNodes, totalFootprint, footprint)
-	if err != nil {
-		return nil, err
-	}
-
-	// ns, err := CalculateAcceptNewSealersReward(nbNodes)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	infl, err := CalculateGlobalInflationControlFactor(totalCryptoAmount)
-	if err != nil {
-		return nil, err
-	}
-	// Reward(n, b) = CarbonReduction(n) * N * GlobalInflationControl(b)
-	rew := new(big.Rat).SetInt(cf)
-	// rew = rew.Add(rew, new(big.Rat).SetInt(ns))
-	rew = rew.Mul(rew, new(big.Rat).SetInt(nbNodes))
-	rew = rew.Mul(rew, infl)
-
-	rewI := new(big.Int).Quo(rew.Num(), rew.Denom())
-	return rewI, nil
-}
-
-func CalculateCarbonFootprintReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int) (*big.Int, error) {
-	if nbNodes.Cmp(zero) == 0 {
-		return nil, errors.New("cannot average with zero node")
-	}
-	if totalFootprint.Cmp(zero) <= 0 {
-		return nil, errors.New("cannot proceed with zero or negative total footprint")
-	}
-	if footprint.Cmp(zero) <= 0 {
-		return nil, errors.New("cannot proceed with zero or negative footprint")
-	}
-	// average = totalFootprint / nbNodes
-	average := new(big.Rat).SetFrac(totalFootprint, nbNodes)
-	// ratio = nbNodes / totalFootprint
-	ratio := new(big.Rat).Inv(average)
-	// ratio = footprint * (nbNodes / totalFootprint) = X
-	ratio = ratio.Mul(ratio, new(big.Rat).SetInt(footprint))
-	// ratio = X + 0,2
-	ratio = ratio.Add(ratio, big.NewRat(2, 10))
-	// ratio = 1 / (X + 0,2)
-	ratio = ratio.Inv(ratio)
-	// ratio = 1 / (X + 0,2) - 0,5
-	ratio = ratio.Sub(ratio, big.NewRat(5, 10))
-	if ratio.Sign() <= 0 {
-		return big.NewInt(0), nil
-	}
-	// reward = 1 CTC (10^18 Wei)
-	reward := new(big.Rat).SetInt(CTCUnit)
-	// reward = ratio * CTC unit
-	reward = reward.Mul(reward, ratio)
-	// convert to big.Int
-	rewardI := new(big.Int).Quo(reward.Num(), reward.Denom())
-	// cap to 2 CTC units
-	cap := big.NewInt(2)
-	cap = cap.Mul(cap, CTCUnit)
-	if rewardI.Cmp(cap) > 0 {
-		rewardI = cap
-	}
-
-	return rewardI, nil
-}
-
-func CalculateAcceptNewSealersReward(nbNodes *big.Int) (*big.Int, error) {
-	// no additional reward when there is one node or less
-	one := big.NewInt(1)
-	if nbNodes.Cmp(one) <= 0 {
-		return zero, nil
-	}
-	// N = nbNodes - 1
-	N := new(big.Rat).SetInt(nbNodes)
-	N = N.Sub(N, big.NewRat(1, 1))
-	// reward = (N-1)/3
-	rew := big.NewRat(1, 3)
-	rew = rew.Mul(N, rew)
-	// reward = (N-1)/3 * CTC Unit
-	rew = rew.Mul(rew, new(big.Rat).SetInt(CTCUnit))
-	// calculate the result rounding to the unit
-	rewI := new(big.Int).Quo(rew.Num(), rew.Denom())
-	return rewI, nil
-}
-
-// Implements the alternative as we have the total amount of crypto created available
-func CalculateGlobalInflationControlFactor(M *big.Int) (*big.Rat, error) {
-	// L = M / (8 000 000 * 30 / 3) // as integer value
-	// D = 2^L // The divisor : 2 at the power of L
-	// GlobalInflationControl = 1/D // 1; 1/2; 1/4; 1/8 ....
-
-	// If there is no crpto created, return 1
-	if M.Cmp(zero) == 0 {
-		return big.NewRat(1, 1), nil
-	}
-	C := big.NewInt(8_000_000 * 30 / 3)
-	C = C.Mul(C, CTCUnit)
-	L := new(big.Rat).SetFrac(M, C)
-	L2 := new(big.Int).Quo(L.Num(), L.Denom()).Uint64()
-	// D = 2^L
-	D := int64(1) << L2
-	// log.Info("Trace CalculateGlobalInflationControlFactor", "M", M, "L2", L2, "D", D)
-	if D == 0 { // The divisor has reached such a large amount (2^63) than the shift gave 0, So Dividing by a very large number is equivalent to 0
-		return big.NewRat(0, 1), nil
-	}
-	return big.NewRat(1, D), nil
-}
-
-type CarbonFootprintContract struct {
-	ContractAddress common.Address
-	RuntimeConfig   *runtime.Config
-}
 
 func NewCarbonFootPrintContract(nodeAddress common.Address, config *params.ChainConfig, state *state.StateDB, header *types.Header) CarbonFootprintContract {
 	contract := CarbonFootprintContract{}
