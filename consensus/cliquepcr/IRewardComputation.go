4c7f9561071485cf7d5cd5ae2c3d566f9115cbd1
sjehan <sjehan@finaxys.com>
2022-10-10 21:27:22 +0200

    Added indexes and interfaces methods

--- consensus/cliquepcr/IRewardComputation.go
+++ consensus/cliquepcr/IRewardComputation.go
@@ -2,11 +2,14 @@ package cliquepcr
 import (
 	"math/big"
 )
+
 type IRewardComputation interface{
 	// Methods
+	GetAlgorithmId() int
 	CalculateAcceptNewSealersReward(nbNodes *big.Int) (*big.Int, error) 
 	CalculateGlobalInflationControlFactor(M *big.Int) (*big.Rat, error)
 	// CalculatePoCRReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, totalCryptoAmount *big.Int) (*big.Int, error)
 	// CalcCarbonFootprintReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, address common.Address, config *params.ChainConfig, state *state.StateDB, header *types.Header) (*big.Int, error)
+	CalculateCarbonFootprintRewardCollection(nodesFootprint []*big.Int, footprint *big.Int, totalCryptoAmount *big.Int) (*big.Int, error) 
 	CalculateCarbonFootprintReward(nbNodes *big.Int, totalFootprint *big.Int, footprint *big.Int, totalCryptoAmount *big.Int) (*big.Int, error) 
 	}
