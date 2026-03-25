56618f122b6fbee9e6aa3b63229a6a1fa429cedb
Guénolé de Cadoudal <guenoledc@yahoo.fr>
2026-03-25 20:15:17 +0100

    Change sync mode from 'snap' to 'full'

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -15,5 +15,5 @@ if [ ! -f .ethereum/geth/LOCK ]; then
 fi
 networkid=$(grep chainId genesis.json | grep -Eo '[0-9]+')
 public_ip=$(/sbin/ip route|awk '/default/ { print $3 }')
-SYNCMODE=snap
+SYNCMODE=full
 geth --networkid $networkid --datadir .ethereum --bootnodes $BOOTNODES --syncmode $SYNCMODE --http --http.addr=0.0.0.0 --http.port=8545 --http.api=web3,eth,net,clique --http.corsdomain=* --http.vhosts=* --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.api=web3,eth,net,clique --ws.origins=* --nat extip:$public_ip
